5cc23f28ce5b1c07af566c76e6436762a61f18de
Ivan Maddalena <73302759+IvanMaddalena@users.noreply.github.com>
2025-05-31 17:47:43 +0200

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,1 +1,43 @@
-# Will be implemented in next steps
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+# Users
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(username=user.username)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+# Notes
+def create_note(db: Session, note: schemas.NoteCreate):
+    db_note = models.Note(**note.dict())
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
+
+def get_notes(db: Session):
+    return db.query(models.Note).all()
+
+def get_note_by_id(db: Session, note_id: int):
+    return db.query(models.Note).filter(models.Note.id == note_id).first()
+
+def update_note(db: Session, note_id: int, note_data: schemas.NoteCreate):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if note:
+        for field, value in note_data.dict().items():
+            setattr(note, field, value)
+        db.commit()
+        db.refresh(note)
+    return note
+
+def delete_note(db: Session, note_id: int):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if note:
+        db.delete(note)
+        db.commit()
+    return note
